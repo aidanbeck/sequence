0d20d75f16916b6b4bf05b92f62e4911ea0c7785
aidanbeck <aidanmbeck@outlook.com>
2026-03-05 21:49:56 -0600

    Grid & Cell classes

--- front-end/sequence.js
+++ front-end/sequence.js
@@ -1,1 +1,22 @@
-alert("test");
+
+class Cell {
+    constructor() {
+        this.number = Math.floor( Math.random() * 10);
+        this.moveDirection = null;
+        this.start = false;
+        this.obstructed = false;
+    }
+}
+
+class Grid {
+    constructor(rows, columns) {
+        this.grid = [];
+
+        for (let i = 0; i < columns; i++) {
+            this.grid[i] = [];
+            for (let j = 0; j < rows; j++) {
+                this.grid[i][j] = new Cell(0);
+            }
+        }
+    }
+}
