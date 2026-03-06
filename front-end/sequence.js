797e77c16a2644ca8e2bfa7155c7851ffdacaba7
aidanbeck <aidanmbeck@outlook.com>
2026-03-05 22:01:20 -0600

    printCells function for testing

--- front-end/sequence.js
+++ front-end/sequence.js
@@ -3,7 +3,6 @@ class Cell {
     constructor() {
         this.number = Math.floor( Math.random() * 9) + 1; // generate random integer between 1 and 9
         this.moveDirection = null;
-        this.start = false;
         this.obstructed = false;
     }
 }
@@ -15,8 +14,22 @@ class Grid {
         for (let i = 0; i < columns; i++) {
             this.grid[i] = [];
             for (let j = 0; j < rows; j++) {
-                this.grid[i][j] = new Cell(0);
+                this.grid[i][j] = new Cell();
             }
+        }
+
+        this.printCells();
+    }
+
+    printCells() {
+
+        for (let i = 0; i < this.grid.length; i++) {
+            let rowString = "";
+            for (let j = 0; j < this.grid[i].length; j++) {
+                let number = this.grid[i][j].number;
+                rowString += `${number}  `;
+            }
+            console.log(`${rowString}\n`);
         }
     }
 }
